15adb83c12a8e8ea180225e15dfe006e1a806183
furoe <17751038730@163.com>
2022-09-15 17:38:49 +0800

    罗马数转数字

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+//import { resolve } from "path";
 
-function pathResolve(dir: string) {
-  return resolve(process.cwd(), ".", dir);
-}
+// function pathResolve(dir: string) {
+//   return resolve(process.cwd(), ".", dir);
+// }
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: pathResolve("src") + "/",
-      },
-      {
-        find: "fn",
-        replacement: pathResolve("src/leetcode") + "/",
-      },
-      {
-        find: "__test__",
-        replacement: pathResolve("src/__test__") + "/",
-      },
-    ],
-  },
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: "@",
+  //       replacement: pathResolve("src") + "/",
+  //     },
+  //     {
+  //       find: "fn",
+  //       replacement: pathResolve("src/leetcode") + "/",
+  //     },
+  //     {
+  //       find: "__test__",
+  //       replacement: pathResolve("src/__test__") + "/",
+  //     },
+  //   ],
+  // },
 });
