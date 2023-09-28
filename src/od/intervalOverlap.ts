2d4b17a644e5ba772fec2608762fca3bf62b4bbc
furoe <17751038730@163.com>
2023-09-28 14:20:33 +0800

    feat: 区间覆盖

--- src/od/intervalOverlap.ts
+++ src/od/intervalOverlap.ts
@@ -1,9 +1,15 @@
 export function intervalOverlap(len: number, arr: number[][]) {
   arr.sort((a, b) => a[0] - b[0]);
-  console.log(arr);
-  const res = [];
-  let base = arr[0];
-  for (let i = 1; i < len; i++) {}
-  console.log(res);
-  return res.length;
+  let res = 1;
+  let max = arr[0][1];
+  for (let i = 0; i < len; i++) {
+    let j = i + 1;
+    while (j < len && arr[j][0] <= arr[i][1]) {
+      max = Math.max(max, arr[j][1]);
+      j++;
+    }
+    res++;
+    i = j - 1;
+  }
+  return res;
 }
